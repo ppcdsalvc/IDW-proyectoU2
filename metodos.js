5b381085a1d10be465c5226f4ef88192229065de
arturoescalante <armiescalanteli@ittepic.edu.mx>
2018-07-03 15:55:21 -0600

    hora

--- metodos.js
+++ metodos.js
@@ -1,6 +1,6 @@
 var primero ='D|00001659|TEPIC, NAY.   |014|002|';
 var fechas ='|22/02/2017';
-var formaPago='|001|';
+var formaPago='|001|   |                $0.00|            ';
 
 
 
@@ -17,9 +17,12 @@ function leer(){
             for(var i=0;i<10;i++){        
             //console.log(i);
             var signo=arreglo[i][72]+'|             ';
+            //asignamos el importe
             var importe=arreglo[i].slice(73,85);
             var puntodec=arreglo[i].slice(85,87)
+            //conertimos importe en entero
             importe= parseInt(importe,10);
+            //conjunto de if para asignar espacios,puntos y comas al importe
             if(importe<10){
                 importe='$'+importe+'.'+puntodec+'     ';
             }else if(importe<100){
@@ -35,9 +38,12 @@ function leer(){
                 var temporal2=arreglo[i].slice(82,85);
                 importe='$'+temporal1+','+temporal2+'.'+puntodec;
             }
-            
+            //Aqui especificamos la hora
+            var h1 = arreglo[i].slice(24,26);
+            var h2=arreglo[i].slice(26,28);
+            var hora ='|'+h1+':'+h2+'|';
         
-            final=final+primero+fechas+fechas+fechas+'|'+signo+importe+formaPago+'\n';
+            final=final+primero+fechas+fechas+fechas+'|'+signo+importe+formaPago+importe+hora+'\n';
             
         }
         console.log(final);
